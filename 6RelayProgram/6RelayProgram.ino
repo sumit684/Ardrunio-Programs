85d9e805abae2ea27105b64b1530d066785e9939
sumit684 <singh.sumit684@gmail.com>
2019-11-01 19:21:02 +0530

    MIT APP INVENTOR AIA included

--- 6RelayProgram/6RelayProgram.ino
+++ 6RelayProgram/6RelayProgram.ino
@@ -13,6 +13,7 @@ const int Relay4 = D4;
 const int Relay5 = D5;
 const int Relay6 = D6;
 const int Relay7 = D7;
+const int Relay8 = D0;
 WiFiServer server(80); // Creates a server that listens for incoming connections on the specified port, here in this case port is 80.
 
 void setup() {
@@ -42,65 +43,72 @@ void loop() {
   client.flush();
 
   // Match the request
-  if (request.indexOf("/req=ON") != -1) {
+  if (request.indexOf("/?req=ON") != -1) {
     digitalWrite(LED_BUILTIN, LOW); //Turns ON
   }
-  if (request.indexOf("/req=OFF") != -1) {
+  if (request.indexOf("/?req=OFF") != -1) {
     digitalWrite(LED_BUILTIN, HIGH); //Turns OFF
   }
-  if (request.indexOf("/req=ON1") != -1) {
+  if (request.indexOf("/?req=ON1") != -1) {
     turnOffAll();
     digitalWrite(Relay1, HIGH);
   }
-  if (request.indexOf("/req=OFF1") != -1) {
+  if (request.indexOf("/?req=OFF1") != -1) {
     digitalWrite(Relay1, LOW);
   }
-  if (request.indexOf("/req=ON2") != -1) {
+  if (request.indexOf("/?req=ON2") != -1) {
     turnOffAll();
     digitalWrite(Relay2, HIGH);
   }
-  if (request.indexOf("/req=OFF2") != -1) {
+  if (request.indexOf("/?req=OFF2") != -1) {
     digitalWrite(Relay2, LOW);
   }
-  if (request.indexOf("/req=ON3") != -1) {
+  if (request.indexOf("/?req=ON3") != -1) {
     turnOffAll();
     digitalWrite(Relay3, HIGH);
   }
-  if (request.indexOf("/req=OFF3") != -1) {
+  if (request.indexOf("/?req=OFF3") != -1) {
     digitalWrite(Relay3, LOW);
   }
-  if (request.indexOf("/req=ON4") != -1) {
+  if (request.indexOf("/?req=ON4") != -1) {
     turnOffAll();
     digitalWrite(Relay4, HIGH);
   }
-  if (request.indexOf("/req=OFF4") != -1) {
+  if (request.indexOf("/?req=OFF4") != -1) {
     digitalWrite(Relay4, LOW);
   }
-  if (request.indexOf("/req=ON5") != -1) {
+  if (request.indexOf("/?req=ON5") != -1) {
     turnOffAll();
     digitalWrite(Relay5, HIGH);
   }
-  if (request.indexOf("/req=OFF5") != -1) {
+  if (request.indexOf("/?req=OFF5") != -1) {
     digitalWrite(Relay5, LOW);
   }
-  if (request.indexOf("/req=ON6") != -1) {
+  if (request.indexOf("/?req=ON6") != -1) {
     turnOffAll();
     digitalWrite(Relay6, HIGH);
   }
-  if (request.indexOf("/req=OFF6") != -1) {
+  if (request.indexOf("/?req=OFF6") != -1) {
     digitalWrite(Relay6, LOW);
   }
-  if (request.indexOf("/req=ON7") != -1) {
+  if (request.indexOf("/?req=ON7") != -1) {
     turnOffAll();
-    digitalWrite(Relay6, HIGH);
+    digitalWrite(Relay7, HIGH);
   }
-  if (request.indexOf("/req=OFF7") != -1) {
-    digitalWrite(Relay6, LOW);
+  if (request.indexOf("/?req=OFF7") != -1) {
+    digitalWrite(Relay7, LOW);
   }
-  if (request.indexOf("/req=OFFALL") != -1) {
+  if (request.indexOf("/?req=ON8") != -1) {
+    turnOffAll();
+    digitalWrite(Relay8, HIGH);
+  }
+  if (request.indexOf("/?req=OFF8") != -1) {
+    digitalWrite(Relay8, LOW);
+  }
+  if (request.indexOf("/?req=OFFALL") != -1) {
     turnOffAll();
   }
-  if (request.indexOf("/req=ONALL") != -1) {
+  if (request.indexOf("/?req=ONALL") != -1) {
     turnOnAll();
   }
 
@@ -120,7 +128,7 @@ void loop() {
   client.println("<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css\">");
   client.println("<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js\"></script>");
   client.println("<script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js\"></script>");
-client.println("</head>");
+  client.println("</head>");
   client.print("Led pin is now: ");
 
   //  if (value == HIGH) {
@@ -129,20 +137,23 @@ client.println("</head>");
   //    client.print("Off");
   //  }
   client.println("<br><br>");
-  client.println("<a href=\"/req=ON1\"\"><button class=\"btn btn-primary m-2\">Turn ON Relay 1 </button></a>");
-  client.println("<a href=\"/req=OFF1\"\"><button class=\"btn btn-primary m-2\">Turn Off Relay 1 </button></a><br />");
-  client.println("<a href=\"/req=ON2\"\"><button class=\"btn btn-primary m-2\">Turn ON Relay 2 </button></a>");
-  client.println("<a href=\"/req=OFF2\"\"><button class=\"btn btn-primary m-2\">Turn Off Relay 2 </button></a><br />");
-  client.println("<a href=\"/req=ON3\"\"><button class=\"btn btn-primary m-2\">Turn ON Relay 3 </button></a>");
-  client.println("<a href=\"/req=OFF3\"\"><button class=\"btn btn-primary m-2\">Turn Off Relay 3 </button></a><br />");
-  client.println("<a href=\"/req=ON4\"\"><button class=\"btn btn-primary m-2\">Turn ON Relay 4 </button></a>");
-  client.println("<a href=\"/req=OFF4\"\"><button class=\"btn btn-primary m-2\">Turn Off Relay 4 </button></a><br />");
-  client.println("<a href=\"/req=ON5\"\"><button class=\"btn btn-primary m-2\">Turn ON Relay 5 </button></a>");
-  client.println("<a href=\"/req=OFF5\"\"><button class=\"btn btn-primary m-2\">Turn Off Relay 5 </button></a><br />");
-  client.println("<a href=\"/req=ON6\"\"><button class=\"btn btn-primary m-2\">Turn ON Relay 6 </button></a>");
-  client.println("<a href=\"/req=OFF6\"\"><button class=\"btn btn-primary m-2\">Turn Off Relay 6 </button></a><br />");
-  client.println("<a href=\"/req=ON7\"\"><button class=\"btn btn-primary m-2\">Turn ON Relay 7 </button></a>");
-  client.println("<a href=\"/req=OFF7\"\"><button class=\"btn btn-primary m-2\">Turn Off Relay 7 </button></a><br />");
+  client.println("<a href=\"/?req=ON1\"\"><button class=\"btn btn-primary m-2\">Turn ON Relay 1 </button></a>");
+  client.println("<a href=\"/?req=OFF1\"\"><button class=\"btn btn-primary m-2\">Turn Off Relay 1 </button></a><br />");
+  client.println("<a href=\"/?req=ON2\"\"><button class=\"btn btn-primary m-2\">Turn ON Relay 2 </button></a>");
+  client.println("<a href=\"/?req=OFF2\"\"><button class=\"btn btn-primary m-2\">Turn Off Relay 2 </button></a><br />");
+  client.println("<a href=\"/?req=ON3\"\"><button class=\"btn btn-primary m-2\">Turn ON Relay 3 </button></a>");
+  client.println("<a href=\"/?req=OFF3\"\"><button class=\"btn btn-primary m-2\">Turn Off Relay 3 </button></a><br />");
+  client.println("<a href=\"/?req=ON4\"\"><button class=\"btn btn-primary m-2\">Turn ON Relay 4 </button></a>");
+  client.println("<a href=\"/?req=OFF4\"\"><button class=\"btn btn-primary m-2\">Turn Off Relay 4 </button></a><br />");
+  client.println("<a href=\"/?req=ON5\"\"><button class=\"btn btn-primary m-2\">Turn ON Relay 5 </button></a>");
+  client.println("<a href=\"/?req=OFF5\"\"><button class=\"btn btn-primary m-2\">Turn Off Relay 5 </button></a><br />");
+  client.println("<a href=\"/?req=ON6\"\"><button class=\"btn btn-primary m-2\">Turn ON Relay 6 </button></a>");
+  client.println("<a href=\"/?req=OFF6\"\"><button class=\"btn btn-primary m-2\">Turn Off Relay 6 </button></a><br />");
+  client.println("<a href=\"/?req=ON7\"\"><button class=\"btn btn-primary m-2\">Turn ON Relay 7 </button></a>");
+  client.println("<a href=\"/?req=OFF7\"\"><button class=\"btn btn-primary m-2\">Turn Off Relay 7 </button></a><br />");
+  client.println("<a href=\"/?req=ON8\"\"><button class=\"btn btn-primary m-2\">Turn ON Relay 8 </button></a>");
+  client.println("<a href=\"/?req=OFF8\"\"><button class=\"btn btn-primary m-2\">Turn Off Relay 8 </button></a><br />");
+  client.println("<a href=\"/?req=OFFALL\"\"><button class=\"btn btn-primary m-2\">Turn Off All </button></a><br />");
   client.println("</html>");
 
   delay(1);
